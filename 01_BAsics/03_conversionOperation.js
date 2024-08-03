95cfa9537a0a534c70bfcefb5acdf01ab387794b
NIKHIL KUMAR SAHU <152062240+Nick2opxd@users.noreply.github.com>
2024-08-03 20:08:28 +0000

    made some conversion bw datatypes

---
+++ 01_BAsics/03_conversionOperation.js
@@ -0,0 +1,46 @@
+// let num=33
+// console.log(typeof(num))
+// console.log(typeof(String(num)))
+
+// let str="33abc"
+// console.log(typeof(str))
+// let strConversion=Number(str)
+// console.log(typeof(strConversion))
+// console.log(strConversion)
+
+let bln=true
+console.log(typeof(bln))
+let blnConversion=Number(bln)
+console.log(typeof(blnConversion))
+console.log(blnConversion)
+
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
