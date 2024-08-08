b868a2190f9cdf11afd26607c7f6fedec5daa980
NIKHIL KUMAR SAHU <152062240+Nick2opxd@users.noreply.github.com>
2024-08-08 20:27:56 +0000

    math.random explained

---
+++ 01_BAsics/06_math.js
@@ -0,0 +1,15 @@
+// let num=100.88
+// let numFixed=num.toFixed(3)//converts the num to string and returns upto to passed argument decimal
+// console.log(numFixed)
+// console.log(typeof(numFixed))
+// console.log(num.toPrecision(4))
+
+// console.log(Math)
+console.log(Math.random())
+console.log(Math.floor(Math.random()*10)+1)
+
+
+let max=15
+let min=5
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
