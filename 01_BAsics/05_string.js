f9806435ab6371c7f238729f966145086a57a44b
NIKHIL KUMAR SAHU <152062240+Nick2opxd@users.noreply.github.com>
2024-08-08 15:37:03 +0000

    Added string methods

---
+++ 01_BAsics/05_string.js
@@ -0,0 +1,21 @@
+let str1="H,i,m,e,s,h"
+let str2="Reshamiya"
+
+let str3=new String('Amitabh')
+
+// console.log(`Names aka chamans ${str1},${str2},${str3}`)
+// // console.log(typeof(str1))
+// console.log(str3.__proto__)
+// console.log(str3.toLowerCase())
+
+// console.log(str3.charAt(4))
+// console.log(str3.indexOf('m'))
+
+// console.log(str1.slice(0,4))
+// console.log(str1.substring(0,2))
+// console.log(str1.split(','))
+
+//today we will discuss about various string methods.these methods helps the user to use the predefined methods to use them in their code.some of the common methods used by developers are as follows: toUpperCase(),toLpperCase(),substring(),split(),trim().
+//we cant pass negative values into substring() but we can pass or negative values are applicable with slice() method only. 
+
+console.log(str1.replace(',','-'))
