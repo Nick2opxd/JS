dce3fb5b504ab9a9075c9c11b5cd30b88aaa3f40
NIKHIL KUMAR SAHU <152062240+Nick2opxd@users.noreply.github.com>
2024-08-09 06:01:50 +0000

    date.js added

---
+++ 01_BAsics/07_date.js
@@ -0,0 +1,15 @@
+// const mydate=new Date("05-08-2024");
+// console.log(mydate.toLocaleString())
+
+const newDate=new Date();
+// console.log(newDate)
+// console.log(newDate.toLocaleString())
+// console.log(newDate.toDateString())
+// console.log(newDate.toLocaleDateString())
+// console.log(newDate.toJSON())
+
+console.log(newDate.toISOString())
+console.log(newDate.getDate())
+console.log(newDate.getDay())
+
+
